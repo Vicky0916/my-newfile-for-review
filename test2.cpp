0b7e60894d1f788c99fe586636fe50b0b5236382
Vickey0916 <271982953@qq.com>
2020-04-06 22:56:15 +0800

    commit

---
+++ test2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string str;
+	while(getline(cin, str))
+	{
+		size_t start = 0;
+		for (int i = 0; i < str.size(); i++)
+		{
+
+		}
+	}
+}
