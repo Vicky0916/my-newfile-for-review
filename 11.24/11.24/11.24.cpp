87c658ecbe0c83ded25aeec5b5d0ea9f9a161206
Vickey0916 <271982953@qq.com>
2020-04-12 23:37:45 +0800

    commit

---
+++ 11.24/11.24/11.24.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+//int CountNumber(int n)
+//{
+//	int sum = 0;
+//	int flag = 0;
+//	for (int i = 1; i <= n / 2; i++)
+//	{
+//		if (n%i == 0)
+//		{
+//			sum += i;
+//	   }
+//     }
+//	if (sum == n)
+//	{
+//		flag=1;
+//	}
+//	else 
+//	{
+//		flag = 0;
+//	}
+//	return flag;
+//}
+
+string CompareCard(string str)
+{
+
+}
+int main()
+{
+	/*int num;
+	int sum=0;
+	while (cin >> num)
+	{
+		for (int i = 1; i <= num; i++)
+		{
+			sum += CountNumber(i);
+		}
+		cout << sum << endl;
+		sum = 0;
+	}*/
+	
+	system("pause");
+	return 0;
+}
