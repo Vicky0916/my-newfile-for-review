077798d6ebbd456275269727e0941f03f6398e1f
Vickey0916 <271982953@qq.com>
2020-04-18 23:25:52 +0800

    commit

---
+++ 向数组插入新数字.py
@@ -0,0 +1,5 @@
+for i in range(0,100):
+    for j in range(0,100):
+        for k in range(0,100): 
+            if ((3*i+2*j+0.5*k==100) and (i+j+k==100)):
+                    print("大马有：",i,"匹，中马有：",j,"匹，小马有：",k ,"匹")
